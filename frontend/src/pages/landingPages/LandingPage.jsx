8258ae51b0926fd1f6d7de6a7c7500170b8462ba
krssachinn@gmail.com <krssachinn@gmail.com>
2025-11-29 00:21:08 +0530

    Fix: Typo error in import of Hero.jsx from LandingPage.jsx

--- frontend/src/pages/landingPages/LandingPage.jsx
+++ frontend/src/pages/landingPages/LandingPage.jsx
@@ -2,7 +2,7 @@ import Faqs from "../../components/landing/Faqs";
 import Features from "../../components/landing/Features";
 import Footer from "../../components/landing/Footer";
 import Header from "../../components/landing/Header";
-import Hero from "../../components/landing/HEro";
+import Hero from "../../components/landing/Hero";
 import Testimonials from "../../components/landing/Testimonials";
 
 const LandingPage = () => {
