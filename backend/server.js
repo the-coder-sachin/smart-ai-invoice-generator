918199913bf1de389cc115eff1799074d6e673dc
krssachinn@gmail.com <krssachinn@gmail.com>
2025-11-29 00:31:01 +0530

    Feat: Add cors policy restriction

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "*",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
